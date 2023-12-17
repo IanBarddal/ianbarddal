ba1bfe7bcd6283bffe83fe27b0264354c1ddd1e7
Ian Barddal <136401651+IanBarddal@users.noreply.github.com>
2023-12-16 22:10:40 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
-#include <locale.h>
-
-float conversor (float t_C);
+#include <string.h>
 
 int main ()
 {
-    setlocale (LC_ALL, "portuguese");
+    int x;
 
-    float t_C;
-    printf ("Digite a temperatura em graus Celsius: ");
-    scanf ("%f", &t_C);
+    char comida [20];
+    char bebida [20];
 
-    conversor (t_C);
+    fgets (comida, 20, stdin);
 
-    // a função "conversor" está EM FUNÇÃO de t_C, ou seja, do valor da temperatura em Celsius digitado pelo usuário.
+    size_t len = strlen(comida);
+    if (len > 0 && comida[len - 1] == '\n')
+        {
+            comida[len - 1] = '\0';
+        }
 
-    printf ("%1.0f graus em Celsius corresponde a %1.1f Fahrenheit!", t_C, conversor (t_C));
+    fgets (bebida, 20, stdin);
 
-    // a função conversor RETORNA o valor da conversão, então, %1.1f corresponde ao valor retornado por "conversor (t_C)", como especificado acima.
+    size_t lenB = strlen(bebida);
+    if (lenB > 0 && bebida[lenB - 1] == '\n')
+        {
+            bebida[lenB - 1] = '\0';
+        }
+
+    if (strcasecmp (comida,"Lasanha") == 0 && strcasecmp (bebida, "Refrigerante") == 0)
+    {
+        printf ("11.00");
+    }
+    else if (strcasecmp (comida, "Lasanha") == 0 && strcasecmp (bebida, "Suco") == 0)
+    {
+        printf ("10.50");
+    }
+    else if (strcasecmp (&comida, "Estrogonofe") == 0 && strcasecmp (&bebida, "Suco") == 0)
+    {
+        printf ("13.50");
+    }
+    else if (strcasecmp (&comida, "Estrogonofe") == 0 && strcasecmp (&bebida, "Refrigerante") == 0)
+    {
+        printf ("14.00");
+    }
 
     return (0);
-}
 
-float conversor (float t_C)
-{
-    float x;
-
-    x = (t_C *9/5) + 32;
-    return (x);
 }
